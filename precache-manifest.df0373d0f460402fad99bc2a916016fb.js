0ee527069a7a053eb728d98de006e32f4b8acf88
Andrey Zhestkov <ajestkov@gmail.com>
2020-02-10 03:40:52 +0300

    Updates

---
+++ precache-manifest.df0373d0f460402fad99bc2a916016fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d72ec8ce03bd4cba2963cff3d694466",
+    "url": "/wire-challenge/index.html"
+  },
+  {
+    "revision": "38e5b5dab7d70dc95b73",
+    "url": "/wire-challenge/static/css/main.6fdc40af.chunk.css"
+  },
+  {
+    "revision": "150beb8b131e0b9852f2",
+    "url": "/wire-challenge/static/js/2.4da2fe45.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/wire-challenge/static/js/2.4da2fe45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38e5b5dab7d70dc95b73",
+    "url": "/wire-challenge/static/js/main.242560d1.chunk.js"
+  },
+  {
+    "revision": "fd02528b81672cf0bc51",
+    "url": "/wire-challenge/static/js/runtime-main.c9d37aff.js"
+  },
+  {
+    "revision": "647966732acbba31af05efe46a095549",
+    "url": "/wire-challenge/static/media/arrow_down.64796673.svg"
+  }
+]);
