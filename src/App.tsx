15d93428a4d00807c67e22e069de6da3b301c392
Andrey Zhestkov <ajestkov@gmail.com>
2020-02-10 10:37:36 +0300

    remove redundant code

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.scss';
 import Container from './components/root/Container/Container';
+import styles from './App.module.scss';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <div className="App">
+      <div className={styles.app}>
         <Router>
           <Switch>
             <Route component={Container} />
